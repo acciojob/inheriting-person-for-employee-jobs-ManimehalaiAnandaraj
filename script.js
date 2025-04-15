557eb102c0714c67d923479f314aeb057a185ed7
ManimehalaiAnandaraj <manimehalaiannakodi@gmail.com>
2025-04-15 20:55:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,21 +1,21 @@
-// complete this js code
 class Person {
 	constructor(name, age) {
 		this.name = name;
 		this.age = age;
 	}
-	 greet(){
+	
+	greet() {
 		console.log(`Hello, my name is ${this.name}, I am ${this.age} years old.`);
 	}
 }
 
-class Employee extends Person{
+class Employee extends Person {
 	constructor(name, age, jobTitle) {
-		super(name,age);
-		this.jobTitle=jobTitle;
+		super(name, age);
+		this.jobTitle = jobTitle;
 	}
-	
-	 jobGreet(){
+
+	jobGreet() {
 		console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`);
 	}
 }
@@ -24,4 +24,5 @@ class Employee extends Person{
 window.Person = Person;
 window.Employee = Employee;
 
+// Log the Employee class
 console.log(Employee);
