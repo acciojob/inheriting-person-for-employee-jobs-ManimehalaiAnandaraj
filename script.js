e3db2b0aa8abaa249c3d7a1a2da9a24ce15de7ae
ManimehalaiAnandaraj <manimehalaiannakodi@gmail.com>
2025-04-16 12:26:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -32,6 +32,5 @@ person.greet();  // This will log: Hello, my name is Alice, I am 25 years old.
 const employee = new Employee("Bob", 30, "Manager");
 employee.jobGreet();  // This will log: Hello, my name is Bob, I am 30 years old, and my job title is Manager.
 
-
 // Log the Employee class
 console.log(Employee);
