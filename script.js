fb60cce90e84e6d184a667ce9ba1529cd7af33c2
ManimehalaiAnandaraj <manimehalaiannakodi@gmail.com>
2025-04-16 12:22:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -24,5 +24,14 @@ class Employee extends Person {
 window.Person = Person;
 window.Employee = Employee;
 
+// Create an instance of Person and call greet
+const person = new Person("Alice", 25);
+person.greet();  // This will log: Hello, my name is Alice, I am 25 years old.
+
+// Create an instance of Employee and call jobGreet
+const employee = new Employee("Bob", 30, "Manager");
+employee.jobGreet();  // This will log: Hello, my name is Bob, I am 30 years old, and my job title is Manager.
+
+
 // Log the Employee class
 console.log(Employee);
